c4597c4ffdd9e628a077f9f952d40718e18abb85
Kreijstal <kreijstal@hotmail.com>
2024-08-17 15:16:37 +0200

    Update installlinux.bat

--- installlinux.bat
+++ installlinux.bat
@@ -3,5 +3,5 @@ start %~dp0grub2win\G2WInstall.exe
 echo Press any key after grub2win installation is complete.
 pause
 xcopy /s /i "%~dp0grub2" "C:\grub2" /Y
-xcopy /s /i "%~dp0linuxmint-21.2-xfce-64bit.iso" "C:\" /Y
+xcopy /s /i "%~dp0linuxmint-22-cinnamon-64bit.iso" "C:\" /Y
 shutdown /r
